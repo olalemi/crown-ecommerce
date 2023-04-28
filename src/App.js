53a87ed8310a3c269c9d9b7af04433c88cd4993f
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-04-28 20:13:35 +0100

    adding files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="catergories-container">
+      <div className="catergory-container">
+        {/* {<img/>} */}
+        <div className="catergory-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
