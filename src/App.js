caa6b38eb087bece79f3e9ed52823f226700356c
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-04-30 10:46:52 +0100

    adding categories

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
+  const categories =[
+ {
+  id:1,
+  title:"Hats",
+  
+ },
+ {
+  id:2,
+  title:"Jackets",
+  
+ },
+ {
+  id:3,
+  title:"Sneakers",
+  
+ },
+ {
+  id:4,
+  title:"Womens",
+  
+ },
+ {
+  id:5,
+  title:"Mens",
+  
+ }
+
+
+  ]
+
   return (
     <div className="catergories-container">
-      <div className="catergory-container">
-        {/* {<img/>} */}
-        <div className="catergory-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ title }) => (
+        <div className="catergory-container">
+            <div className="background-image"></div>
+          <div className="catergory-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-     
+      ))}
     </div>
   );
 }
